ab5baac621caa09b1dc4a7e9a1c178ebf603ea7a
Matt Martindale <marty2327@gmail.com>
2020-11-08 22:43:34 -0600

    First round of changes, expand info, add wind commpass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import keys from "./keys";
-
+import arrow from "./img/arrow.png";
 const api = {
   key: keys.API_KEY,
   base: keys.BASE_URL,
@@ -17,7 +17,7 @@ function App() {
   const [weather, setWeather] = useState({});
   const search = (e) => {
     if (e.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      fetch(`${api.base}weather?q=${query  + ", US"}&units=imperial&APPID=${api.key}`)
         .then((res) => res.json())
         .then((result) => {
           setQuery("");
@@ -31,7 +31,7 @@ function App() {
     <div
       className={
         typeof weather.main != "undefined"
-          ? weather.main.temp > 18
+          ? weather.main.temp > 32
             ? "App hot"
             : "App cold"
           : "App"
@@ -41,7 +41,7 @@ function App() {
         <div className="search-container">
           <input
             type="text"
-            placeholder="Search..."
+            placeholder="Search City, State ..."
             className="search-bar"
             onChange={(e) => setQuery(e.target.value)}
             value={query}
@@ -52,15 +52,28 @@ function App() {
           <div>
             <div className="location-container">
               <div className="location">
-                {weather.name}, {weather.sys.country}
+                {weather.name}, {weather.state}, {weather.sys.country}
               </div>
               <div className="date"> {dateBuild(new Date())}</div>
             </div>
             <div className="weather-container">
-              <div className="temperature">
-                {Math.round(weather.main.temp)}°C
+              <div className="weather-icon">
+                <img src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`}
+                  alt='Icon depicting current weather.' />
               </div>
-              <div className="weather">{weather.weather[0].main}</div>
+              <div className="temperature">
+                {Math.round(weather.main.temp)}°F
+                <p>Feels Like {Math.round(weather.main.feels_like)}°F</p>
+              </div>
+              <div className="weather">Conditions: {weather.weather[0].main}</div>
+              <div className="weather">Wind Speed {Math.round(weather.wind.speed)} MPH</div>
+              <div className="weather">
+                Wind Direction 
+                <div className="compass">
+                  <img src={arrow} style= {{width:"40px", transform: `rotate(${weather.wind.deg - 180}deg)`}}
+                    alt='compass arrow' />
+                </div>
+              </div>
             </div>
           </div>
         ) : (
